d7026c58e87d22681889ba90d1178b16356a393c
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-10-18 18:30:01 -0200

    [4.2]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-root',
-  template: `
-    <p>
-      app Works!
-    </p>
-  `,
-  styles: []
+  templateUrl: `./app.component.html`
 })
 export class AppComponent {
   title = 'app';
+  fotos: Object[] = []
+
+  constructor(ajax: Http){
+    ajax.get('http://localhost:3000/v1/fotos')
+        .subscribe(
+          resposta => this.fotos = resposta.json()
+          , erro => console.log(erro)
+        )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    PainelModule,
+    FotoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
